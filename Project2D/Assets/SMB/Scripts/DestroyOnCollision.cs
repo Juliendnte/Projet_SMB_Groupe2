2cf29c0d1b31dd9dca3f433d980d2df6a8ca6a70
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2025-02-14 13:30:40 +0100

    feat (script) : break wall

---
+++ Project2D/Assets/SMB/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DestroyBlock : MonoBehaviour
+{
+    // Permet de choisir le Layer via l'inspecteur
+    [SerializeField] private LayerMask blockBreakLayerMask;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // On vérifie que le layer de l'objet touché est dans le LayerMask
+        if ((blockBreakLayerMask.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            // Détruit uniquement l'objet touché
+            Destroy(collision.gameObject);
+        }
+    }
+}
